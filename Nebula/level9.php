0b2d11fe4378d41741d587a3b63e39d8940219d6
caiqiqi <564933437@qq.com>
2016-11-12 21:08:28 +0800

    update level9.php

--- Nebula/level9.php
+++ Nebula/level9.php
@@ -2,6 +2,8 @@
 
 function spam($email)
 {
+  # functio preg_replace($pattern, $replacement, $subject)
+  # 搜索$subject中匹配$pattern的部分，  以$replacement进行替换。
   $email = preg_replace("/\./", " dot ", $email);
   $email = preg_replace("/@/", " AT ", $email);
   
@@ -10,6 +12,8 @@ function spam($email)
 
 function markup($filename, $use_me)
 {
+  # fucntion file_get_contents($filename)
+  # 将整个文件读入一个字符串
   $contents = file_get_contents($filename);
 
   $contents = preg_replace("/(\[email (.*)\])/e", "spam(\"\\2\")", $contents);
